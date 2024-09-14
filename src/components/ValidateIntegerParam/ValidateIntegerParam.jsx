b4e3d4961d4e547748b7761d33d39ca6127e6aff
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-09-14 19:37:15 -0300

    SCRUM-72 Update documentation comment to ValidateIntegerParam

--- src/components/ValidateIntegerParam/ValidateIntegerParam.jsx
+++ src/components/ValidateIntegerParam/ValidateIntegerParam.jsx
@@ -4,6 +4,10 @@ import { Navigate, useParams } from 'react-router-dom';
  * Component that validates a URL parameter as a non-negative integer.
  * If the parameter is invalid, it redirects to a "Not Found" page.
  *
+ * NOTE: This component validates that the URL parameter is a non-negative integer.
+ * After validation, the parameter will be accessible as a string in child components 
+ * via the useParams() hook, even though it was validated as an integer here.
+ * 
  * @param {Object} props - The props object.
  * @param {ReactNode} props.children - The child components to render if the parameter is valid.
  * @param {string} props.paramName - The name of the URL parameter to validate.
