abd386275fe7daee8f31109d39fb2371d7619a68
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-09-14 19:13:17 -0300

    SCRUM-72 Add documentation comments to components and custom hooks

--- src/components/ValidateIntegerParam/ValidateIntegerParam.jsx
+++ src/components/ValidateIntegerParam/ValidateIntegerParam.jsx
@@ -1,5 +1,15 @@
 import { Navigate, useParams } from 'react-router-dom';
 
+/**
+ * Component that validates a URL parameter as a non-negative integer.
+ * If the parameter is invalid, it redirects to a "Not Found" page.
+ *
+ * @param {Object} props - The props object.
+ * @param {ReactNode} props.children - The child components to render if the parameter is valid.
+ * @param {string} props.paramName - The name of the URL parameter to validate.
+ *
+ * @returns {ReactNode} - The children components if the parameter is valid, or a <Navigate /> to the Not Found page if invalid.
+ */
 const ValidateIntegerParam = ({ children, paramName }) => {
   const paramString = useParams()[paramName];
 
--- src/hooks/useRouteNavigation.js
+++ src/hooks/useRouteNavigation.js
@@ -1,5 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 
+/**
+ * Custom hook for handling navigation between different routes.
+ *
+ * @returns {Object} Object containing functions to navigate to different routes.
+ */
 const useRouteNavigation = () => {
   const navigate = useNavigate();
 
