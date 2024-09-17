8a0bdba675e7283aee409baecc735f1d799d9d1b
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-09-17 19:31:14 -0300

    SCRUM-80 Texto del button en negro

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ text, onPress, type = 'button' }) => {
     <button
       type={type}
       onClick={onPress}
-      className='p-3 rounded-xl bg-white hover:bg-black hover:text-white transition-all'
+      className='p-3 rounded-xl bg-white text-black hover:bg-black hover:text-white transition-all'
     >
       {text}
     </button>
