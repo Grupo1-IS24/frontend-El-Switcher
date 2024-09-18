a5aead1c02d170999b639a10f0c6c2ca2e6b56b5
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-09-18 15:58:10 -0300

    SCRUM-30 Página de listado de partidas integrando los componentes necesarios

--- src/pages/GameListPage.jsx
+++ src/pages/GameListPage.jsx
@@ -1,38 +1,27 @@
-import GameCard from '../components/GameCard/GameCard';
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
+import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
+import GameGrid from '../components/GameGrid/GameGrid';
 import useGetGameList from '../hooks/useGetGameList';
+import Button from '../components/Button/Button';
 
 const GameListPage = () => {
-  const { gameList, isLoading, error } = useGetGameList();
-
-  if (isLoading) {
-    return (
-      <div className='flex justify-center items-center h-screen'>
-        <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-200'></div>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className='flex justify-center items-center h-screen'>
-        <div className='bg-red-500 text-white p-5 rounded-lg shadow-lg animate-bounce'>
-          Error al cargar las partidas: {error.message}
-        </div>
-      </div>
-    );
-  }
+  const { gameList, isLoading, error, refreshGameList } = useGetGameList();
 
   return (
-    <div className='grid gap-10 p-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
-      {gameList.map((game) => (
-        <div className='flex justify-center' key={game.gameId}>
-          <GameCard
-            gameName={game.gameName}
-            maxPlayers={game.maxPlayers}
-            connectedPlayers={game.connectedPlayers}
-          />
+    <div className='relative'>
+      {!isLoading && (
+        <div className='absolute top-4 left-4'>
+          <Button text='🗘' onPress={refreshGameList} />
         </div>
-      ))}
+      )}
+
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : error ? (
+        <ErrorMessage error={error} onRetry={refreshGameList} />
+      ) : (
+        <GameGrid gameList={gameList} />
+      )}
     </div>
   );
 };
