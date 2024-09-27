af6a78f725661315e4e1bd5bc91a05c57d7b3df6
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-09-27 18:54:07 -0300

    SCRUM-41 Agregado de estilos y atributo disabled en el componente button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,10 @@
-const Button = ({ text, onPress, style, type = 'button' }) => {
+const Button = ({ 
+  text, 
+  onPress, 
+  style,
+  type = 'button',
+  isDisabled = false
+}) => {
   // define the classes for each button style
   const baseClasses = 'lekton-bold rounded-xl transition-all';
   const typeClasses = {
@@ -8,8 +14,10 @@ const Button = ({ text, onPress, style, type = 'button' }) => {
       'text-xl p-4 bg-white text-black hover:bg-black hover:text-white',
     borderButton:
       'text-xl bg-white text-black px-4 py-1 mt-2 border-white border-2 hover:bg-transparent hover:text-white hover:border-white',
-    lobbyButton_disabled: 'cursor-not-allowed',
-    lobbyButton_init: '',
+    lobbyButton_disabled:
+      'w-[18rem] h-[4.375rem] text-3xl border-2 border-[#f1f1f1] bg-[#f1f1f1] text-[#C0C0C0] cursor-not-allowed disabled',
+    lobbyButton_init: 
+      'w-[18rem] h-[4.375rem] text-3xl border-2 border-[#f1f1f1] bg-[#f1f1f1] text-[#0c0c0c] hover:bg-transparent hover:text-[#f1f1f1]',
     lobbyButton_abandone: '',
     gameButton_pass: '',
     gameButton_abandone: '',
@@ -19,7 +27,7 @@ const Button = ({ text, onPress, style, type = 'button' }) => {
   const buttonClasses = `${baseClasses} ${typeClasses[style]}`;
 
   return (
-    <button type={type} onClick={onPress} className={buttonClasses}>
+    <button type={type} onClick={onPress} className={buttonClasses} disabled={isDisabled}>
       {text}
     </button>
   );
