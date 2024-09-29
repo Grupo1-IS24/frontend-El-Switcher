9a163599795c097cbbe933a4cebf3d420858b6d3
Heber Alturria <heber.alturria.git@gmail.com>
2024-09-29 14:21:29 -0300

    SCRUM-59 Add logic to show player turn

--- src/components/DisplayPlayers/DisplayPlayers.jsx
+++ src/components/DisplayPlayers/DisplayPlayers.jsx
@@ -1,12 +1,13 @@
 import PlayerInfo from '../PlayerInfo/PlayerInfo';
 
-const DisplayPlayers = ({ listOfPlayers }) => {
+const DisplayPlayers = ({ listOfPlayers, playerTurnId }) => {
   return (
     <>
       {listOfPlayers.map((player, index) => (
         <PlayerInfo
           playerName={player.playerName}
           index={index}
+          isTurn={player.playerId === playerTurnId}
           key={player.playerId}
         />
       ))}
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -3,11 +3,14 @@ import useWebsocketGame from '../hooks/useWebsocketGame';
 import Board from '../components/Board/Board';
 
 const GamePage = () => {
-  const { listOfPlayers, board } = useWebsocketGame();
+  const { listOfPlayers, board, playerTurnId } = useWebsocketGame();
 
   return (
     <>
-      <DisplayPlayers listOfPlayers={listOfPlayers} />
+      <DisplayPlayers
+        listOfPlayers={listOfPlayers}
+        playerTurnId={playerTurnId}
+      />
       <Board board={board} />
     </>
   );
