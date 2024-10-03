40b66cc78e59b2f23c3eaf87c3ec64b3f3d9e7fa
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-03 00:16:50 -0300

    SCRUM-104 ErrorMessage cambiado por MessageCard para generalizar

---
+++ src/components/MessageCard/MessageCard.jsx
@@ -0,0 +1,16 @@
+const MessageCard = ({ type, message }) => {
+  const typeClasses = {
+    error: 'bg-red-500',
+    info: 'bg-[#0C0C0C]',
+  };
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className={`${typeClasses[type]} font-semibold text-white text-xl p-5 rounded-lg shadow-lg animate-bounce`}>
+        {message}
+      </div>
+    </div>
+  );
+};
+
+export default MessageCard;
