629ac522ded13c3894a61dc4678c75b596da1efc
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-07 01:46:09 -0300

    SCRUM-143 Refactor del código LobbyCard

--- src/components/LobbyCard/LobbyCard.jsx
+++ src/components/LobbyCard/LobbyCard.jsx
@@ -7,6 +7,34 @@ import useGetGame from '../../hooks/useGetGame';
 import { useParams } from 'react-router-dom';
 import LoadingLobby from '../LoadingLobby/LoadingLobby';
 
+const PlayerInfo = ({ listOfPlayers, minPlayers, maxPlayers }) => (
+  <div className='flex flex-col gap-0'>
+    <p className='lekton-bold text-4xl'>
+      Jugadores conectados: {listOfPlayers.length}
+    </p>
+    <p className='lekton-bold text-4xl'>Mín. jugadores: {minPlayers}</p>
+    <p className='lekton-bold text-4xl'>Max. jugadores: {maxPlayers}</p>
+  </div>
+);
+
+const OwnerActions = ({ canStartGame }) => (
+  <div className='flex flex-row gap-5 justify-center'>
+    <StartGameButton isDisabled={!canStartGame} />
+    <LeaveButton type={'lobby'} />
+  </div>
+);
+
+const NonOwnerActions = () => (
+  <div className='flex flex-col gap-3'>
+    <p className='lekton-bold text-2xl text-[#60d394]'>
+      Esperando que el owner comience la partida...
+    </p>
+    <div className='flex justify-center'>
+      <LeaveButton type={'lobby'} />
+    </div>
+  </div>
+);
+
 const LobbyCard = () => {
   const { gameId } = useParams();
   const { listOfPlayers, canStartGame } = useWebsocketLobby();
@@ -20,32 +48,15 @@ const LobbyCard = () => {
   return (
     <div className='bg-[#0c0c0c] rounded-xl text-[#f1f1f1] text-center flex flex-col gap-16 px-8 py-12 max-w-3xl m-auto'>
       <h2 className='lekton-bold text-6xl underline'>{game.gameName}</h2>
-      <div className='flex flex-col gap-0'>
-        <p className='lekton-bold text-4xl'>
-          Jugadores conectados: {listOfPlayers.length}
-        </p>
-        <p className='lekton-bold text-4xl'>
-          Mín. jugadores: {game.minPlayers}
-        </p>
-        <p className='lekton-bold text-4xl'>
-          Max. jugadores: {game.maxPlayers}
-        </p>
-      </div>
-
+      <PlayerInfo
+        listOfPlayers={listOfPlayers}
+        minPlayers={game.minPlayers}
+        maxPlayers={game.maxPlayers}
+      />
       {isOwner ? (
-        <div className='flex flex-row gap-5 justify-center'>
-          <StartGameButton isDisabled={!canStartGame} />
-          <LeaveButton type={'lobby'} />
-        </div>
+        <OwnerActions canStartGame={canStartGame} />
       ) : (
-        <div className='flex flex-col gap-3'>
-          <p className='lekton-bold text-2xl text-[#60d394]'>
-            Esperando que el owner comience la partida...
-          </p>
-          <div className='flex justify-center'>
-            <LeaveButton type={'lobby'} />
-          </div>
-        </div>
+        <NonOwnerActions />
       )}
     </div>
   );
