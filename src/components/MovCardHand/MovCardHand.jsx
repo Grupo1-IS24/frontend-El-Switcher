8e97c63365550a959c27e46e8a535d849f6915a4
nachogonzaliaunc <ignacio.gonzalia@mi.unc.edu.ar>
2024-10-09 20:41:20 -0300

    SCRUM-136 Arreglo el bug de tener hijos con el mismo key

--- src/components/MovCardHand/MovCardHand.jsx
+++ src/components/MovCardHand/MovCardHand.jsx
@@ -4,17 +4,18 @@ import { GameContext } from '../../contexts/GameProvider';
 
 const MovCardHand = ({ playerId }) => {
   const { movementCards, listOfPlayers } = useContext(GameContext);
+  let invalidCardsCount = 0;
 
   return (
     <>
       <div className='flex flex-row gap-6'>
         {/* player with index 0 is always the player in the window */}
         {listOfPlayers[0].playerId === playerId
-          ? movementCards.map((movementCard, index) => (
-              <MovementCard key={index} movement={movementCard.moveType} />
+          ? movementCards.map((movementCard) => (
+              <MovementCard key={movementCard.movementcardId} movement={movementCard.moveType} />
             ))
-          : movementCards.map((index) => (
-              <MovementCard key={index} movement={'invalid'} />
+          : movementCards.map(() => (
+              <MovementCard key={`invalid-${invalidCardsCount++}`} movement={'invalid'} />
             ))}
       </div>
     </>
