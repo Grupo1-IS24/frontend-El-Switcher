27e4b57354fbadb80d0224330da3a1b52ce95078
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-10-11 20:17:26 -0300

    SCRUM-136 Fix MovCardHand logic

--- src/components/MovCardHand/MovCardHand.jsx
+++ src/components/MovCardHand/MovCardHand.jsx
@@ -2,29 +2,18 @@ import MovementCard from '../MovementCard/MovementCard';
 import { useContext } from 'react';
 import { GameContext } from '../../contexts/GameProvider';
 
-const MovCardHand = ({ playerId }) => {
-  const { movementCards, listOfPlayers } = useContext(GameContext);
-  let invalidCardsCount = 0;
+const MovCardHand = () => {
+  const { movementCards } = useContext(GameContext);
 
   return (
-    <>
-      <div className='flex flex-row gap-6'>
-        {/* player with index 0 is always the player in the window */}
-        {listOfPlayers[0].playerId === playerId
-          ? movementCards.map((movementCard) => (
-              <MovementCard
-                key={movementCard.movementcardId}
-                movement={movementCard.moveType}
-              />
-            ))
-          : movementCards.map(() => (
-              <MovementCard
-                key={`invalid-${invalidCardsCount++}`}
-                movement={'invalid'}
-              />
-            ))}
-      </div>
-    </>
+    <div className='flex flex-row gap-6'>
+      {movementCards.map((movementCard) => (
+        <MovementCard
+          key={movementCard.movementcardId}
+          movement={movementCard.moveType}
+        />
+      ))}
+    </div>
   );
 };
 
