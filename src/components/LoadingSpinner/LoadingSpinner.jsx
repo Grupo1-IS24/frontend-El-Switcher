6acfeb4233e5a39a495994bed55bf16c46678ec9
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-13 21:02:13 -0300

    SCRUM-161 Agregado de un data-testid en el BgOverlay y LoadingSpinner para tests

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,5 +1,5 @@
 const LoadingSpinner = () => (
-  <div className='flex justify-center items-center h-screen'>
+  <div className='flex justify-center items-center h-screen' data-testid='loading-spinner'>
     <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-200'></div>
   </div>
 );
