1b6857d161b4591d7e8d168b24a487972538f71a
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-10-15 18:50:22 -0300

    SCRUM-118 Add logic to display in gray scale a movement card if was played

--- src/components/MovCardHand/MovCardHand.jsx
+++ src/components/MovCardHand/MovCardHand.jsx
@@ -2,11 +2,13 @@ import MovementCard from '../MovementCard/MovementCard';
 import { useContext } from 'react';
 import { GameContext } from '../../contexts/GameProvider';
 import { PlayMovementLogicContext } from '../../contexts/PlayMovementLogicProvider';
+import usePlayedMovCards from '../../hooks/usePlayedMovCards';
 
 const MovCardHand = () => {
   const { movementCards } = useContext(GameContext);
   const { selectMovementCard, isSelectedMovementCard, canSelectMovementCard } =
     useContext(PlayMovementLogicContext);
+  const { isMovementCardPlayed } = usePlayedMovCards();
 
   return (
     <div className='flex flex-row gap-6'>
@@ -17,6 +19,7 @@ const MovCardHand = () => {
           isSelected={isSelectedMovementCard(movementCard)}
           disabled={!canSelectMovementCard()}
           onClick={() => selectMovementCard(movementCard)}
+          isPlayed={isMovementCardPlayed(movementCard)}
         />
       ))}
     </div>
