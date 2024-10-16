5e3fa91f7bf07129d5770d308fb8ccb0018515f3
nachogonzaliaunc <ignacio.gonzalia@mi.unc.edu.ar>
2024-10-16 19:36:19 -0300

    SCRUM-166 Resaltar figuras en tablero

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,15 +1,24 @@
 import { useContext } from 'react';
 import { PlayMovementLogicContext } from '../../contexts/PlayMovementLogicProvider';
+import { GameContext } from '../../contexts/GameProvider';
 import ColorCard from '../ColorCard/ColorCard';
 
 const Board = ({ board }) => {
   const { selectColorCard, canSelectColorCard, isSelectedColorCard } =
     useContext(PlayMovementLogicContext);
 
+  const { foundFigures } = useContext(GameContext);
+
+  const isPartOfFigure = (index) => {
+    return foundFigures.some(figure =>
+      figure.some(chip => chip.row * 6 + chip.col === index)
+    );
+  };
+
   return (
     <div className='fixed h-screen w-screen'>
       <div className='flex justify-center items-center h-screen'>
-        <div className='grid grid-cols-6 grid-rows-6'>
+        <div className='grid grid-cols-6 grid-rows-6 gap-2'>
           {board.map((colorCard, index) => (
             <ColorCard
               key={index}
@@ -17,6 +26,7 @@ const Board = ({ board }) => {
               onClick={() => selectColorCard(colorCard)}
               disabled={!canSelectColorCard(colorCard)}
               isSelected={isSelectedColorCard(colorCard)}
+              isPartOfFigure={isPartOfFigure(index)}
             />
           ))}
         </div>
