f99d6557a6bc341ccd611aab9ca8628c7a81114a
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-16 20:19:43 -0300

    SCRUM-121 Agregado re robustes en el servicio de cancelar movimiento

--- src/service/CancelMovementService.js
+++ src/service/CancelMovementService.js
@@ -1,17 +1,46 @@
+import axios from 'axios';
 import { apiService } from './axiosConfig';
+import { z } from 'zod';
 
+// Schema to validate the data for canceling a movement.
+const cancelMovementSchema = z.object({
+  gameID: z.number().int().nonnegative(),
+  playerID: z.number().int().nonnegative(),
+});
+
+/**
+ * Function to cancel a movement. It sends a request to the server to cancel a movement.
+ *
+ * @param {number} gameID - The game identifier.
+ * @param {number} playerID - The player identifier.
+ * @returns {Promise<void>} A promise that resolves if the movement was canceled successfully, otherwise rejects with an error.
+ * @throws {Error} If the data is invalid or an error occurs while canceling the movement.
+ */
 export const cancelMovement = async (gameID, playerID) => {
-  if (gameID === null || playerID === null) {
-    throw new Error('gameID o playerID no están definidos');
-  }
+  const cancelMovementBody = { gameID, playerID };
 
   try {
+    cancelMovementSchema.parse(cancelMovementBody);
+
     const response = await apiService.post(
       `/game/${gameID}/move_undo/${playerID}`
     );
     return response;
   } catch (error) {
-    console.error(`error al cancelar el movimiento: ${error}`);
-    throw new Error('Error cancelando el movimiento');
+    if (error instanceof z.ZodError) {
+      console.error('Error validando datos de cancelación:', error);
+      throw new Error('Datos de cancelación inválidos');
+    }
+
+    if (axios.isAxiosError(error)) {
+      console.error('Error cancelando el movimiento:', error);
+      throw new Error(
+        error.response?.data?.detail ??
+          'Error desconocido en la respuesta del servidor'
+      );
+    }
+
+    console.error('Error inesperado cancelando el movimiento:', error);
+    throw new Error('Error inesperado cancelando el movimiento');
   }
 };
