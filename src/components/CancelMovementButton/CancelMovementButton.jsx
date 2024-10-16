67d91ff968f7f97d8fb728ab35fc6a187e316453
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-16 20:10:38 -0300

    SCRUM-121 Servicio de cancelar movimiento integrado al botón correspondiente

--- src/components/CancelMovementButton/CancelMovementButton.jsx
+++ src/components/CancelMovementButton/CancelMovementButton.jsx
@@ -1,9 +1,22 @@
-import { useContext } from 'react';
 import Button from '../Button/Button';
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { PlayerContext } from '../../contexts/PlayerProvider';
+import { cancelMovement } from '../../service/CancelMovementService';
 import { PlayMovementLogicContext } from '../../contexts/PlayMovementLogicProvider';
 
 const CancelMovementButton = () => {
+  const { gameId } = useParams();
+  const { playerID } = useContext(PlayerContext);
   const { canCancelMovement } = useContext(PlayMovementLogicContext);
+
+  const handleCancelMovement = async () => {
+    try {
+      await cancelMovement(gameId, playerID);
+    } catch (error) {
+      console.error('Error cancelando movimiento:', error);
+    }
+  };
 
   return (
     <>
@@ -11,9 +24,7 @@ const CancelMovementButton = () => {
         <Button
           text={'Cancelar movimiento'}
           style={'gameButton_cancelMovement'}
-          onPress={() => {
-            console.log('Movimiento cancelado');
-          }}
+          onPress={handleCancelMovement}
         />
       )}
     </>
