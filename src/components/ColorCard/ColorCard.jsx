01be81bb4882f35a7b8b8ecbe1b2e8e0ceec8fd1
gag <gabriel.solis.guimpelevich@mi.unc.edu.ar>
2024-10-19 20:06:58 -0300

    SCRUM-186 Added wiggle animation when selecting pieces Changed the tailwind.config file and the isSelected conditional in ColorCard.

--- src/components/ColorCard/ColorCard.jsx
+++ src/components/ColorCard/ColorCard.jsx
@@ -17,7 +17,8 @@ const ColorCard = ({
       className={`w-24 h-24
         ${colorStyle[color] ?? 'bg-gray-500'} rounded
         ${disabled ? 'cursor-not-allowed' : ''}
-        ${isSelected ? 'transform scale-90 shadow-inner border-4 border-white transition-all duration-200' : 'scale-100 transition-all duration-200'}
+        ${isSelected ? 'animate-shriggle' 
+                     : 'scale-100 transition-all duration-500'}
         ${isPartOfFigure ? 'border-4' : ''}`}
       disabled={disabled}
       onClick={onClick}
