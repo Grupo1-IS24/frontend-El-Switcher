b9f3e0e2310ad60b6c6cfad8585792413c2c72d5
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-21 18:26:34 -0300

    SCRUM-193 Centrado del componente LoadingSpinner

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => (
   <div
-    className='flex justify-center items-center h-screen'
+    className='fixed inset-0 flex justify-center items-center'
     data-testid='loading-spinner'
   >
     <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-200'></div>
