d860796dba6815dcb38d1d1cd84642cf1888a8eb
Heber Alturria <heber.alturria.git@gmail.com>
2024-10-26 17:01:16 -0300

    SCRUM-205 Modify WinnerMessage tests

--- src/components/WinnerMessage/WinnerMessage.test.jsx
+++ src/components/WinnerMessage/WinnerMessage.test.jsx
@@ -2,6 +2,7 @@ import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import WinnerMessage from './WinnerMessage';
+import useWinnerPlayer from '../../hooks/useWinnerPlayer';
 
 const mockRedirectToHomePage = vi.fn();
 
@@ -12,46 +13,95 @@ vi.mock('../../hooks/useRouteNavigation', () => ({
   }),
 }));
 
+// Mock useWinnerPlayer.
+vi.mock('../../hooks/useWinnerPlayer');
+
 describe('WinnerMessage', () => {
-  const setup = (winnerName) =>
-    render(<WinnerMessage winnerName={winnerName} />);
+  const setup = (
+    thereIsWinner = true,
+    isCurrentPlayerWinner = true,
+    winnerName = ''
+  ) => {
+    useWinnerPlayer.mockReturnValue({
+      isCurrentPlayerWinner,
+      thereIsWinner,
+      winnerName,
+    });
+
+    render(<WinnerMessage />);
+  };
+
+  const getHomeButton = () => screen.queryByText('Ir al inicio');
 
   afterEach(() => {
     vi.clearAllMocks(); // Clear all mocks after each test.
   });
 
-  describe('when winnerName is provided', () => {
+  describe('when there is not a winner', () => {
     beforeEach(() => {
-      setup('Edward Elric');
+      setup(false, false);
     });
 
-    it('renders the winner message with the name', () => {
-      expect(screen.getByText('¡Ganaste! Edward Elric')).toBeInTheDocument();
+    it('should not render the winner message', () => {
+      expect(
+        screen.queryByText('¡Felicidades, ganaste!')
+      ).not.toBeInTheDocument();
+
+      expect(screen.queryByText('¡Perdiste ante !')).not.toBeInTheDocument();
+
+      expect(screen.queryByText('🏆')).not.toBeInTheDocument();
+
+      expect(screen.queryByText('😞')).not.toBeInTheDocument();
+
+      expect(getHomeButton()).not.toBeInTheDocument();
     });
   });
 
-  describe('when winnerName is null', () => {
+  describe('when there is a winner and it is the current player', () => {
     beforeEach(() => {
-      setup(null);
+      setup();
     });
 
-    it('renders the winner message without a name', () => {
-      expect(screen.getByText('¡Ganaste!')).toBeInTheDocument();
+    it('should render the winner message', () => {
+      expect(screen.queryByText('¡Felicidades, ganaste!')).toBeInTheDocument();
+      expect(screen.queryByText('🏆')).toBeInTheDocument();
+
+      expect(screen.queryByText('¡Perdiste ante !')).not.toBeInTheDocument();
+      expect(screen.queryByText('😞')).not.toBeInTheDocument();
+    });
+  });
+
+  describe('when there is a winner and it is not the current player', () => {
+    beforeEach(() => {
+      setup(true, false, 'Edward Elric');
+    });
+
+    it('should renders the winner message', () => {
+      expect(
+        screen.queryByText('¡Felicidades, ganaste!')
+      ).not.toBeInTheDocument();
+
+      expect(
+        screen.queryByText('¡Perdiste ante Edward Elric!')
+      ).toBeInTheDocument();
+
+      expect(screen.queryByText('🏆')).not.toBeInTheDocument();
+
+      expect(screen.queryByText('😞')).toBeInTheDocument();
     });
   });
 
   describe('Button functionality', () => {
     beforeEach(() => {
-      setup('Edward Elric');
+      setup();
     });
 
-    it('renders the "Ir al inicio" button', () => {
-      expect(screen.getByText('Ir al inicio')).toBeInTheDocument();
+    it('should render the "Ir al inicio" button when there is a winner', () => {
+      expect(getHomeButton()).toBeInTheDocument();
     });
 
-    it('calls redirectToHomePage when button is clicked', async () => {
-      const button = screen.getByText('Ir al inicio');
-      await userEvent.click(button);
+    it('should call redirectToHomePage when the "Ir al inicio" button is clicked', async () => {
+      await userEvent.click(getHomeButton());
       expect(mockRedirectToHomePage).toHaveBeenCalledTimes(1);
     });
   });
