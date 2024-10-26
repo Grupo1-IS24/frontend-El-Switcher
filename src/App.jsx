28c24f5281c66c20a06f2bb83a5375f70ba33d9e
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-26 18:46:05 -0300

    SCRUM-199 Integrado del ToastContainer en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import PlayerProvider from './contexts/PlayerProvider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <PlayerProvider>
       <RouterProvider router={router} />
+      <ToastContainer/>
     </PlayerProvider>
   );
 };
