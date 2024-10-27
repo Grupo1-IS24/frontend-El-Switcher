2ea288809babd1ba41e8bf4d0bb25684ba577a7b
Heber Alturria <heber.alturria.git@gmail.com>
2024-10-27 14:53:58 -0300

    SCRUM-211 Refactor code to add default prop values

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,11 +1,16 @@
-const TextInput = ({ name, placeholder = '', value, onChange }) => {
+const TextInput = ({
+  name,
+  placeholder = '',
+  value = undefined,
+  onChange = () => {},
+}) => {
   return (
     <input
       type='text'
       name={name}
       placeholder={placeholder}
-      {...(onChange ? { onChange } : {})}
-      {...(value !== undefined && value !== null ? { value } : {})}
+      value={value}
+      onChange={onChange}
       className='w-full px-4 py-2 rounded-lg bg-gray-700 text-gray-200 placeholder-gray-400 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-amber-500'
     />
   );
