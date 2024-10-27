02da5a5f94b390f7e20fb3933d9c175fcf7ae870
Heber Alturria <heber.alturria.git@gmail.com>
2024-10-27 18:36:55 -0300

    SCRUM-211 Add useCallback in the useFilterGameList

--- src/hooks/useFilterGameList.js
+++ src/hooks/useFilterGameList.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import { FilterGameListContext } from '../contexts/FilterGameListProvider';
 import showToast from '../utils/toastUtil';
 
@@ -13,75 +13,85 @@ const useFilterGameList = () => {
     resetFilter,
   } = useContext(FilterGameListContext);
 
-  const handleByConnectedPlayers = (
-    event,
-    setFunction,
-    comparisonFn,
-    comparisonErrorMessage
-  ) => {
-    const value = parseInt(event.target.value);
+  const handleByConnectedPlayers = useCallback(
+    (event, setFunction, comparisonFn, comparisonErrorMessage) => {
+      const value = parseInt(event.target.value);
 
-    if (isNaN(value)) {
-      setFunction('');
-      return;
-    }
+      if (isNaN(value)) {
+        setFunction('');
+        return;
+      }
 
-    if (value < 1 || value > 4) {
-      showToast({
-        type: 'error',
-        message: `El valor debe estar entre 1 y 4.`,
+      if (value < 1 || value > 4) {
+        showToast({
+          type: 'error',
+          message: `El valor debe estar entre 1 y 4.`,
+        });
+        return;
+      }
+
+      if (comparisonFn(value)) {
+        showToast({
+          type: 'error',
+          message: comparisonErrorMessage,
+        });
+        return;
+      }
+
+      setFunction(value);
+    },
+    []
+  );
+
+  const handleSearchGameName = useCallback(
+    (event) => {
+      setSearchGameName(event.target.value);
+    },
+    [setSearchGameName]
+  );
+
+  const handleSearchMinPlayers = useCallback(
+    (event) => {
+      handleByConnectedPlayers(
+        event,
+        setSearchMinPlayers,
+        (value) => searchMaxPlayers !== '' && value > searchMaxPlayers,
+        'El número mínimo de jugadores conectados no puede ser mayor al máximo de jugadores conectados.'
+      );
+    },
+    [handleByConnectedPlayers, searchMaxPlayers, setSearchMinPlayers]
+  );
+
+  const handleSearchMaxPlayers = useCallback(
+    (event) => {
+      handleByConnectedPlayers(
+        event,
+        setSearchMaxPlayers,
+        (value) => searchMinPlayers !== '' && value < searchMinPlayers,
+        'El número máximo de jugadores conectados no puede ser menor al mínimo de jugadores conectados.'
+      );
+    },
+    [handleByConnectedPlayers, searchMinPlayers, setSearchMaxPlayers]
+  );
+
+  const filterGameList = useCallback(
+    (gameList) => {
+      return gameList.filter((game) => {
+        const minPlayersCondition =
+          searchMinPlayers === '' || game.connectedPlayers >= searchMinPlayers;
+
+        const maxPlayersCondition =
+          searchMaxPlayers === '' || game.connectedPlayers <= searchMaxPlayers;
+
+        const nameCondition = game.gameName
+          .toLowerCase()
+          .startsWith(searchGameName.toLowerCase());
+
+        return minPlayersCondition && maxPlayersCondition && nameCondition;
       });
-      return;
-    }
-
-    if (comparisonFn(value)) {
-      showToast({
-        type: 'error',
-        message: comparisonErrorMessage,
-      });
-      return;
-    }
-
-    setFunction(value);
-  };
-
-  const handleSearchGameName = (event) => {
-    setSearchGameName(event.target.value);
-  };
-
-  const handleSearchMinPlayers = (event) => {
-    handleByConnectedPlayers(
-      event,
-      setSearchMinPlayers,
-      (value) => searchMaxPlayers !== '' && value > searchMaxPlayers,
-      'El número mínimo de jugadores conectados no puede ser mayor al máximo de jugadores conectados.'
-    );
-  };
-
-  const handleSearchMaxPlayers = (event) => {
-    handleByConnectedPlayers(
-      event,
-      setSearchMaxPlayers,
-      (value) => searchMinPlayers !== '' && value < searchMinPlayers,
-      'El número máximo de jugadores conectados no puede ser menor al mínimo de jugadores conectados.'
-    );
-  };
-
-  const filterGameList = (gameList) => {
-    return gameList.filter((game) => {
-      const minPlayersCondition =
-        searchMinPlayers === '' || game.connectedPlayers >= searchMinPlayers;
-
-      const maxPlayersCondition =
-        searchMaxPlayers === '' || game.connectedPlayers <= searchMaxPlayers;
-
-      const nameCondition = game.gameName
-        .toLowerCase()
-        .startsWith(searchGameName.toLowerCase());
-
-      return minPlayersCondition && maxPlayersCondition && nameCondition;
-    });
-  };
+    },
+    [searchGameName, searchMinPlayers, searchMaxPlayers]
+  );
 
   return {
     searchGameName,
