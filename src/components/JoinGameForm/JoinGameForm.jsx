63da8cccae46f5f5f8b0e82fbec20d2023c12254
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-10-28 23:22:47 -0300

    SCRUM-237 gamePassword en JoinGameForm dependiendo si el game isPublic

--- src/components/JoinGameForm/JoinGameForm.jsx
+++ src/components/JoinGameForm/JoinGameForm.jsx
@@ -1,7 +1,12 @@
 import TextInput from '../TextInput/TextInput';
 
-const JoinGameForm = () => (
-  <TextInput name='playerName' placeholder='Ingresa tu nombre' />
+const JoinGameForm = ({ isPublic }) => (
+  <>
+    <TextInput name='playerName' placeholder='Ingresa tu nombre' />
+    {isPublic && (
+      <TextInput name='gamePassword' placeholder='Ingresa la contraseña' />
+    )}
+  </>
 );
 
 export default JoinGameForm;
