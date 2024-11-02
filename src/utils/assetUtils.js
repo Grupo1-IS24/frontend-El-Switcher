34f3d710173fd9856ae9c05c0b0c7941e9bc7430
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-11-01 22:50:56 -0300

    SCRUM-229 Improve documentation

--- src/utils/assetUtils.js
+++ src/utils/assetUtils.js
@@ -4,6 +4,12 @@ import {
   VALID_BLOCKED_COLORS,
 } from '../constants/assetConstants';
 
+/**
+ * Checks if the provided color is a valid blocked color.
+ *
+ * @param {string} blockedColor - The color to validate.
+ * @returns {boolean} - Returns `true` if the color is valid and exists in the predefined list of blocked colors, `false` otherwise.
+ */
 const isValidBlockedColor = (blockedColor) => {
   return (
     blockedColor &&
@@ -13,10 +19,10 @@ const isValidBlockedColor = (blockedColor) => {
 };
 
 /**
- * Returns the image path for a blocked color card based on the provided color.
+ * Constructs the file path for the blocked color card image based on the provided color.
  *
- * @param {string} blockedColor - The color of the blocked color in lowercase.
- * @returns {string} - The full image path for the blocked color card, or an empty string if no valid color is provided (e.g., if the input is not a string).
+ * @param {string} blockedColor - The color for the blocked card image. Should be a valid string in lowercase.
+ * @returns {string} - The complete image file path for the blocked color card if the color is valid; otherwise, returns an empty string.
  */
 export const getBlockedCardImagePath = (blockedColor) => {
   if (!isValidBlockedColor(blockedColor)) return '';
