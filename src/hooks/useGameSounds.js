4931452fbbaa7d430e12d7c6d0c53419e8559300
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-03 00:55:22 -0300

    SCRUM-258 hook useGameSounds creado para la reproducción de los sonidos

---
+++ src/hooks/useGameSounds.js
@@ -0,0 +1,18 @@
+import useSound from 'use-sound';
+import loser from '../../assets/Sounds/loser.mp3';
+import winner from '../../assets/Sounds/winner.mp3';
+
+export const useGameSounds = () => {
+  const [playLoser, { stop: stopLoser }] = useSound(loser);
+  const [playWinner, { stop: stopWinner }] = useSound(winner);
+
+  const playSound = (isWinner) => {
+    isWinner ? playWinner() : playLoser();
+  };
+
+  const stopSound = (isWinner) => {
+    isWinner ? stopWinner() : stopLoser();
+  };
+
+  return { playSound, stopSound };
+};
