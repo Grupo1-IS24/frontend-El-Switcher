8bf94127f697508f62259e7ab4cc22061bbf8d9f
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-11-03 18:34:02 -0300

    SCRUM-231 Refactor useScrollToBottom

--- src/hooks/useScrollToBottom.js
+++ src/hooks/useScrollToBottom.js
@@ -1,7 +1,41 @@
 import { useEffect, useRef } from 'react';
 
+const scrollToBottom = (containerRef) => {
+  containerRef.current.scrollTop = containerRef.current.scrollHeight;
+};
+
+const canScroll = (scrollHeight, scrollTop, offsetHeight, threshold) => {
+  return scrollHeight <= scrollTop + offsetHeight + threshold;
+};
+
+const easeLinear = (currentTime, startValue, changeInValue, duration) => {
+  return (changeInValue * currentTime) / duration + startValue;
+};
+
+const smoothScroll = (containerRef, start, end, duration) => {
+  const change = end - start;
+  const increment = 20;
+  let currentTime = 0;
+
+  const animateScroll = () => {
+    currentTime += increment;
+    containerRef.current.scrollTop = easeLinear(
+      currentTime,
+      start,
+      change,
+      duration
+    );
+
+    if (currentTime < duration) {
+      requestAnimationFrame(animateScroll);
+    }
+  };
+
+  animateScroll();
+};
+
 /**
- * This hook scrolls to the bottom of the container smoothly and only when necessary.
+ * This hook scrolls to the bottom of the container smoothly on initialization and only when necessary on updates.
  *
  * @param {any} dependency The dependency to watch for changes.
  * @param {number} threshold The distance from the bottom to trigger scrolling.
@@ -14,38 +48,21 @@ const useScrollToBottom = (
   scrollDuration = 1000
 ) => {
   const containerRef = useRef(null);
+  const hasScrolledInitially = useRef(false);
 
   useEffect(() => {
-    // Easing function for smooth scrolling.
-    const easeLinear = (currentTime, startValue, changeInValue, duration) =>
-      (changeInValue * currentTime) / duration + startValue;
+    if (!containerRef.current) return;
 
-    if (containerRef.current) {
-      const { offsetHeight, scrollHeight, scrollTop } = containerRef.current;
+    if (!hasScrolledInitially.current) {
+      scrollToBottom(containerRef);
+      hasScrolledInitially.current = true;
+      return;
+    }
 
-      // If the user is close to the bottom of the container, scroll to the bottom
-      if (scrollHeight <= scrollTop + offsetHeight + threshold) {
-        const start = scrollTop;
-        const change = scrollHeight - start;
-        const increment = 20;
-        let currentTime = 0;
+    const { offsetHeight, scrollHeight, scrollTop } = containerRef.current;
 
-        const animateScroll = () => {
-          currentTime += increment;
-          containerRef.current.scrollTop = easeLinear(
-            currentTime,
-            start,
-            change,
-            scrollDuration
-          );
-
-          if (currentTime < scrollDuration) {
-            requestAnimationFrame(animateScroll);
-          }
-        };
-
-        animateScroll();
-      }
+    if (canScroll(scrollHeight, scrollTop, offsetHeight, threshold)) {
+      smoothScroll(containerRef, scrollTop, scrollHeight, scrollDuration);
     }
   }, [dependency, threshold, scrollDuration]);
 
