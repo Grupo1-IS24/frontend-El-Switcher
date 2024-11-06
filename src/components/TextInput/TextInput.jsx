733ba2d85c5f44c8884283d6f42cf052781f5216
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-06 18:52:22 -0300

    SCRUM-235 Cambio disabled habilitado

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -3,6 +3,7 @@ const TextInput = ({
   placeholder = '',
   value = undefined,
   onChange = () => {},
+  disabled = false,
 }) => {
   return (
     <input
@@ -11,7 +12,8 @@ const TextInput = ({
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className='w-full px-4 py-2 rounded-lg bg-gray-700 text-gray-200 placeholder-gray-400 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-amber-500'
+      disabled={disabled}
+      className={`w-full px-4 py-2 rounded-lg bg-gray-700 text-gray-200 placeholder-gray-400 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-amber-500 ${disabled ? 'cursor-not-allowed' : ''}`}
     />
   );
 };
