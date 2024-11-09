1431ad6247f9d74a80cb767b5f33fffef3e12c1d
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-11-09 20:36:47 -0300

    SCRUM-233 Add a new component to list the messages

---
+++ src/components/MessageList/MessageList.jsx
@@ -0,0 +1,15 @@
+// components/MessageList.js
+import { forwardRef } from 'react';
+
+const MessageList = forwardRef(({ messages, renderMessage }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className='chat-scrollbar flex flex-col h-64 overflow-y-auto space-y-2 break-words'
+    >
+      {messages.map(renderMessage)}
+    </div>
+  );
+});
+
+export default MessageList;
