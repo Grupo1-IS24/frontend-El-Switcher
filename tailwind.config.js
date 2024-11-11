af7b2d2e8edcee31101ecbb43bc02b3591a5983b
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-11 00:13:00 -0300

    SCRUM-274 Agregado de pc como tamaño de ancho de pantalla

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      'pc': '1366px',
+    },
     extend: {
       animation: {
         shriggle: 'shrink 0.2s 1, wiggle 0.75s 0.2s infinite',
