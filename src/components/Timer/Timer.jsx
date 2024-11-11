c4d11929136f84765d3c3936f7ddf45e298aaebc
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-10 22:32:34 -0300

    SCRUM-274 Disminución de tamaño del temporizador para pantallas medias

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,17 +1,17 @@
 import formatTime from '../../utils/formatTime';
 
 const Timer = ({ time }) => {
-  let timeColorClass = 'text-[#60D394]';
+  let timeColorClass = 'text-[#22c55e]';
 
   if (time <= 20) {
-    timeColorClass = 'text-[#FF0000]';
+    timeColorClass = 'text-[#ef4444]';
   } else if (time <= 60) {
-    timeColorClass = 'text-[#FFFF00]';
+    timeColorClass = 'text-[#eab308]';
   }
 
   return (
     <span
-      className={`w-[124px] h-[50px] rounded-[8px] bg-[#0C0C0C] ${timeColorClass} text-[36px] font-semibold flex justify-center`}
+      className={`w-[100px] h-[40px] rounded-[8px] bg-[#0C0C0C] ${timeColorClass} text-[26px] font-semibold flex justify-center`}
     >
       {formatTime(time)}
     </span>
