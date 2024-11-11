2738eedd8d0f320d2549ec60399bc98d96aa9f78
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-11 00:24:44 -0300

    SCRUM-274 Restos de tamaños restaurados

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    screens: {
-      pc: '1368px',
-    },
     extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        'pc': '1368px',
+      },
       animation: {
         shriggle: 'shrink 0.2s 1, wiggle 0.75s 0.2s infinite',
       },
