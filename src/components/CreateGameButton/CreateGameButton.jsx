f77f092e0ec7981e1804628c03be764373b578d3
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-10 22:23:50 -0300

    SCRUM-274 Eliminado del CreateGameForm del CreateGameButton

--- src/components/CreateGameButton/CreateGameButton.jsx
+++ src/components/CreateGameButton/CreateGameButton.jsx
@@ -2,20 +2,14 @@ import { useState } from 'react';
 import Button from '../Button/Button';
 import CreateGameForm from '../CreateGameForm/CreateGameForm';
 
-const CreateGameButton = () => {
-  const [showForm, setShowForm] = useState(false);
-
+const CreateGameButton = ({setShowForm}) => {
   return (
     <>
-      {showForm ? (
-        <CreateGameForm setShowForm={setShowForm} />
-      ) : (
         <Button
           text='Crear partida'
           onPress={() => setShowForm(true)}
           style='homeButton'
         />
-      )}
     </>
   );
 };
